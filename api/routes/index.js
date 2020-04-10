97fee4d9b99a5f1768a1818daaafabc66f04edec
Vasanth Rathnakumar <46497574+Vasanthgigi@users.noreply.github.com>
2020-04-10 18:20:42 -0400

    Bugs

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  debugger;
-
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'IDP-Authentication' });
 });
 
 module.exports = router;
